d06ebae79baf6beae223e7262012251381c2c4f3
CarlosTP97 <carlos.tirado@gruponerio.com>
2021-11-17 14:58:04 -0700

    aws ic

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hola")
+})
+
+app.listen(3000, (err) => {
+    if (err) return console.log(err)
+
+    console.log("Corriendo en el puerto 3000")
+})
